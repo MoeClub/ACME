390093a14a456dff1fb40357fb26502475d5dccd
MoeClub <32381827+MoeClub@users.noreply.github.com>
2024-06-09 17:55:23 +0800

    Update acmeRenew.sh

--- acmeRenew.sh
+++ acmeRenew.sh
@@ -1,33 +1,52 @@
 #!/bin/bash
 
 crtDomain="${1:-}"
-crtSeviceRoot="${2:-nginx:/etc/nginx/crt}"
+crtSeviceRoot="${2:-nginx:/etc/nginx}"
 crtServer="${3:-http://crt.libmk.com}"
 crtCron="${4:-0}"
 crtServiceName=`echo "${crtSeviceRoot}" |cut -d':' -f1`
 crtRoot=`echo "${crtSeviceRoot}" |cut -d':' -f2`
 
-
-[ "${crtCron}" != "0" ] && execPath="/usr/local/bin/acmeRenew.sh" && cp -rf "$0" "$execPath" && chmod 777 "$execPath" && sed -i "/${crtDomain}/d;\$a\25 4 * * 1 root bash ${execPath} ${crtDomain} ${crtRoot} ${crtServer} >/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1
-
+crtTarget="${crtRoot%/}"
+# crtTarget="${crtRoot%/}/${crtDomain%/}"
 
 [ -n "${crtDomain}" ] && [ -n "${crtRoot}" ] && [ -n "${crtServer}" ] || exit 1
-[ -d "${crtRoot%/}/${crtDomain%/}" ] || mkdir -p "${crtRoot%/}/${crtDomain%/}"
+[ "${crtCron}" != "0" ] && execPath="/usr/local/bin/acmeRenew.sh" && cp -rf "$0" "$execPath" && chmod 777 "$execPath" && sed -i "/${crtDomain}/d;\$a\25 4 * * 1 root bash ${execPath} ${crtDomain} ${crtSeviceRoot} ${crtServer} >/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1
 
 
-wget -qO "/tmp/_crt_server.crt.pem" "${crtServer%/}/${crtDomain%/}/server.crt.pem";
+crt="$(mktemp)"
+key="$(mktemp)"
+rm -rf ${crt} ${key}
+trap "rm -rf ${crt} ${key}" EXIT
+
+checkMd5() {
+  crtMode="${1:-}"
+  crtPath="${2:-}"
+  [ -n "$crtMode" ] && [ -n "$crtPath" ] && [ -f "$crtPath" ] || return
+  [ "$crtMode" == "crt" ] && crtMd5=`openssl x509 -in "${crtPath}" -pubkey -noout -outform pem 2>/dev/null |openssl md5 2>/dev/null |cut -d'=' -f2 |grep -o '[0-9a-z]*'`
+  [ "$crtMode" == "key" ] && crtMd5=`openssl pkey -in "${crtPath}" -pubout -outform pem 2>/dev/null |openssl md5 2>/dev/null |cut -d'=' -f2 |grep -o '[0-9a-z]*'`
+  echo -ne "$crtMd5"
+}
+
+# check local crt
+localMd5=`checkMd5 crt "${crtTarget%/}/server.crt.pem"`
+# download crt
+wget -qO "${crt}" "${crtServer%/}/${crtDomain%/}/server.crt.pem";
 [ "$?" -eq "0" ] || exit 1
-crtMd5=`openssl x509 -in "/tmp/_crt_server.crt.pem" -pubkey -noout -outform pem 2>/dev/null |openssl md5 2>/dev/null |cut -d'=' -f2 |grep -o '[0-9a-z]*'`
+crtMd5=`checkMd5 crt "${crt}"`
 [ -n "${crtMd5}" ] || exit 1
-wget -qO "/tmp/_key_server.key.pem" "${crtServer%/}/${crtDomain%/}/server.key.pem";
+[ -n "${localMd5}" ] && [ "${localMd5}" == "${crtMd5}" ] && exit 0
+# download key
+wget -qO "${key}" "${crtServer%/}/${crtDomain%/}/server.key.pem";
 [ "$?" -eq "0" ] || exit 1
-keyMd5=`openssl pkey -in "/tmp/_key_server.key.pem" -pubout -outform pem 2>/dev/null |openssl md5 2>/dev/null |cut -d'=' -f2 |grep -o '[0-9a-z]*'`
+keyMd5=`checkMd5 key "${key}"`
 [ -n "${keyMd5}" ] || exit 1
 [ -n "${crtMd5}" ] && [ -n "${keyMd5}" ] && [ "${crtMd5}" == "${keyMd5}"  ] || exit 1
-[ -f "/tmp/_crt_server.crt.pem" ] && [ -f "/tmp/_key_server.key.pem" ] || exit 1
-cp -rf "/tmp/_crt_server.crt.pem" "${crtRoot%/}/${crtDomain%/}/server.crt.pem"
-cp -rf "/tmp/_key_server.key.pem" "${crtRoot%/}/${crtDomain%/}/server.key.pem"
-rm -rf "/tmp/_crt_server.crt.pem" "/tmp/_key_server.key.pem"
+[ -f "${crt}" ] && [ -f "${key}" ] || exit 1
+# target
+[ -d "${crtTarget%/}" ] || mkdir -p "${crtTarget%/}"
+cp -rf "${crt}" "${crtTarget%/}/server.crt.pem"
+cp -rf "${key}" "${crtTarget%/}/server.key.pem"
 
-for serviceName in "${crtServiceName[@]}"; do systemctl restart "${serviceName}" 2>/dev/null; done
-
+# restart service
+systemctl restart "${crtServiceName}" 2>/dev/null
